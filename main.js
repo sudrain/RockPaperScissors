2df21277cb5f3c91cd967dd87eadba4205ea2a92
sudrain <densedra@gmail.com>
2025-04-22 19:07:10 +0500

    add easy button with dom, without style

--- main.js
+++ main.js
@@ -1,49 +1,78 @@
-function getComputerChoice () {
+const buttons = document.querySelectorAll('button');
+const info = document.querySelector('.info');
+const count = document.querySelector('.count');
+const container = document.querySelector('.container');
+const body = document.querySelector('body');
+
+let humanScore = 0;
+let computerScore = 0;
+
+info.textContent = 'Hello! lets play?'
+buttons.forEach((button) => {
+    button.addEventListener('click', () => { playRound(button.textContent, getComputerChoice()) });
+});
+
+function getComputerChoice() {
     let allChoice = ['rock', 'paper', 'scissors'];
     let choice = Math.floor(Math.random() * 3);
     return allChoice[choice];
 }
 
-function getHumanChoice () {
-    result = window.prompt('get your choice(rock, paper, scissors)', 'scissors');
-    return result.toLowerCase();
+function playRound(humanChoice, computerChoice) {
+
+    console.log(humanChoice + ' vs ' + computerChoice);
+    if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        humanScore += 1;
+        info.textContent = "You : Computer"
+        count.textContent = `${humanScore} : ${computerScore}`
+        gameScore();
+    }
+    if (humanChoice == 'rock' && computerChoice == 'paper') {
+        computerScore += 1;
+        info.textContent = "You : Computer"
+        count.textContent = `${humanScore}    : ${computerScore}`
+        gameScore();
+    }
+    if (humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore += 1;
+        info.textContent = "You : Computer"
+        count.textContent = `${humanScore}    : ${computerScore}`
+        gameScore();
+    }
+    if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        computerScore += 1;
+        info.textContent = "You : Computer"
+        count.textContent = `${humanScore}    : ${computerScore}`
+        gameScore();
+    }
+    if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        humanScore += 1;
+        info.textContent = "You : Computer"
+        count.textContent = `${humanScore}    : ${computerScore}`
+        gameScore();
+    }
+    if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        computerScore += 1;
+        info.textContent = "You : Computer"
+        count.textContent = `${humanScore}    : ${computerScore}`
+        gameScore();
+    }
 }
 
-function playGame () {
 
-    let humanScore = 0;
-    let computerScore = 0;
 
-    function playRound (humanChoice, computerChoice) {
-        console.log(humanChoice + ' vs ' + computerChoice);
-        if (humanChoice == 'rock' && computerChoice == 'scissors') {
-            humanScore += 1;
+function gameScore() {
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore > computerScore) {
+            info.textContent = "let's play?";
+            count.textContent = 'win';
+            humanScore = 0;
+            computerScore = 0;
+        } else {
+            info.textContent = "let's play?";
+            count.textContent = 'defeat'
+            humanScore = 0;
+            computerScore = 0;
         }
-        if (humanChoice == 'rock' && computerChoice == 'paper') {
-            computerScore += 1;
-        }
-        if (humanChoice == 'paper' && computerChoice == 'rock') {
-            humanScore += 1;
-        }
-        if (humanChoice == 'paper' && computerChoice == 'scissors') {
-            computerScore += 1;
-        }
-        if (humanChoice == 'scissors' && computerChoice == 'paper') {
-            humanScore += 1;
-        }
-        if (humanChoice == 'scissors' && computerChoice == 'rock') {
-            computerScore += 1;
-        }
-    }
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log(humanScore + ':' + computerScore);
-    }
-    if (humanScore > computerScore) {
-        console.log('you won ' + humanScore + ':' + computerScore);
-    } else if (computerScore > humanScore) {
-        console.log('you lose ' + humanScore + ':' + computerScore);
-    } else {
-        console.log(humanScore + ':' + computerScore);
     }
 }
